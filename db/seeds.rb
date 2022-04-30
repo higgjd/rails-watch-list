1c8e61b06792297c6eaac2d4c5f69faf710e5d0d
Jenny Higgins <higgjd@gmail.com>
2022-04-30 14:22:52 +0100

    added routes and created lists controller with methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,14 +19,15 @@ base_url = 'https://image.tmdb.org/t/p/original'
 puts 'Seeding...'
 
 1.times do |m|
+  # To read each page of results (doesn't work on the lewagon version of tmdb)
   movies_serialized = URI.open("#{url}?page=#{(m + 1)}").read
   movies = JSON.parse(movies_serialized)['results']
   movies.each do |movie|
     puts "creating #{movie['title']}",
     Movie.create(
-      title: movie['title'],
+      title: movie['title'], # JSONs use strings not keys
       overview: movie['overview'],
-      poster_url: base_url + movie['poster_path'],
+      poster_url: base_url + movie['poster_path'], # Can interpolate
       rating: movie['vote_average']
     )
     puts "#{movie['title']} created"
